3cdfe49edae894f9c4d1477419060ee940db59ea
ayush-ruhr <ayush.pandey@ruhr.agency>
2024-10-09 01:51:58 +0200

    improved init script for setting up vpn in the databricks

--- databricks_cluster_vpn_setup.sh
+++ databricks_cluster_vpn_setup.sh
@@ -1,33 +1,16 @@
 #!/bin/bash
 
 # Define the region and local directory path for the VPN configuration
+#LOCAL_VOLUME="/Users/ayushpandey/work/projects/private-databricks/isolake"
 LOCAL_VOLUME="/Volumes/main/test/test_volume"
-LOG_FILE="/Volumes/main/test/test_volume/vpn_setup.log"
-
-# Ping Google to check internet connectivity
-echo "Pinging Google to check connectivity..." >> $LOG_FILE
-ping -c 1 google.com >> $LOG_FILE
-if [ $? -ne 0 ]; then
-    echo "Ping failed. No internet connectivity." >> $LOG_FILE
-    exit 1
-else
-    echo "Ping successful." >> $LOG_FILE
-fi
+LOG_FILE="$LOCAL_VOLUME/vpn_setup.log"
 
 # Update package manager and install OpenVPN
 echo "Installing OpenVPN" >> $LOG_FILE
-sudo yum update -y
-sudo yum install -y openvpn
+sudo yum update -y >> $LOG_FILE 2>&1
+sudo yum install -y openvpn >> $LOG_FILE 2>&1
 echo "OpenVPN installed successfully" >> $LOG_FILE
 
-
-
-# Copy OpenVPN configuration and static key from the local volume
-echo "Copying OpenVPN configuration from the local volume" >> $LOG_FILE
-cp $LOCAL_VOLUME/client.ovpn /home/ec2-user/client.ovpn
-cp $LOCAL_VOLUME/static.key /home/ec2-user/static.key
-echo "Successfully copied VPN configuration from the local volume" >> $LOG_FILE
-
 # Start the OpenVPN connection
-sudo openvpn --config /home/ec2-user/client.ovpn --daemon
+sudo openvpn --config $LOCAL_VOLUME/client.ovpn --daemon >> $LOG_FILE 2>&1
 echo "OpenVPN started successfully" >> $LOG_FILE
