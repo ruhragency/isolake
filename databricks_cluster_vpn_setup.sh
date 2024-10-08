066f9885f183da6e493a39e2bf488547366cf77a
ayush-ruhr <ayush.pandey@ruhr.agency>
2024-10-08 18:02:47 +0200

    add logs to the set up vpn script

--- databricks_cluster_vpn_setup.sh
+++ databricks_cluster_vpn_setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Update package manager and install OpenVPN
+echo "Installing openvpn"
 sudo yum update -y
 sudo yum install -y openvpn
-
+echo "openvpn installed successfully"
 # Install AWS CLI if not already installed
 if ! command -v aws &> /dev/null
 then
@@ -11,12 +12,15 @@ then
     sudo yum install -y aws-cli
 fi
 
-# Set the region (You can set this in your Terraform variables)
+echo "AWS CLI installed successfully"
+
 REGION=us-east-1  # Change to your region
 
 # Download OpenVPN configuration and static key from S3
+echo "Copying open vpn configurations from s3"
 aws s3 cp s3://private-databricks-data-bucket/init-scripts/client.ovpn /home/ec2-user/client.ovpn --region $REGION
 aws s3 cp s3://private-databricks-data-bucket/init-scripts/static.key /home/ec2-user/static.key --region $REGION
-
+echo "successfully copied vpn configurations from s3"
 # Start the OpenVPN connection
 sudo openvpn --config /home/ec2-user/client.ovpn --daemon
+echo "open vpn stated successfully"
