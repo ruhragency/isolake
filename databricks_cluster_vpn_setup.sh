11740257b10ed63850b18c1c5110e6e13bb59be3
ayush-ruhr <ayush.pandey@ruhr.agency>
2024-10-08 17:23:31 +0200

    set environment variables for the databricks cluster environment

--- databricks_cluster_vpn_setup.sh
+++ databricks_cluster_vpn_setup.sh
@@ -11,6 +11,9 @@ then
     sudo yum install -y aws-cli
 fi
 
+# Set the region (You can set this in your Terraform variables)
+REGION=us-east-1  # Change to your region
+
 # Download OpenVPN configuration and static key from S3
 aws s3 cp s3://private-databricks-data-bucket/init-scripts/client.ovpn /home/ec2-user/client.ovpn --region $REGION
 aws s3 cp s3://private-databricks-data-bucket/init-scripts/static.key /home/ec2-user/static.key --region $REGION
